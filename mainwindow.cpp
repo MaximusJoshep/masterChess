da79d5c90fa46cde0ec263329a367540015c1033
Michael <mapazacals@unsa.edu.pe>
2020-10-14 19:42:35 -0500

    Se añadio la interfaz grafica junto a la imagen del tablero

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,10 @@ MainWindow::MainWindow(QWidget *parent)
   , ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
+  QPixmap chessBoard("/home/michael/QTProjects/masterChess/imagenes/tablero.jpg");
+  int width_board = ui->label_tablero->width();
+  int height_board = ui->label_tablero->height();
+  ui->label_tablero->setPixmap(chessBoard.scaled(400 , 400, Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
